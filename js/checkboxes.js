8286dba2361059bf2a6e1de7f5fcda64cf051a1c
jmealing <31008799+jmealing@users.noreply.github.com>
2019-03-11 08:45:18 -0400

    notes in js

--- js/checkboxes.js
+++ js/checkboxes.js
@@ -7,11 +7,22 @@ $(".btnCheck").click(function(event) {
     }
 });
 
+// need more calls when multi phone lines are on a single page
+
 $(".btnCheck2").click(function(event) {
     if ($('.beforeCall2 input[type="checkbox"]').not(':checked').length == 0) {
 				document.getElementById("check-message2").innerHTML = '<label class="label label-success">You are ready to call</label>';
     } else {
 				document.getElementById("check-message2").innerHTML = '<label class="label label-danger">You need all of the above to call</label>';
+
+    }
+});
+
+$(".btnCheck3").click(function(event) {
+    if ($('.beforeCall3 input[type="checkbox"]').not(':checked').length == 0) {
+				document.getElementById("check-message3").innerHTML = '<label class="label label-success">You are ready to call</label>';
+    } else {
+				document.getElementById("check-message3").innerHTML = '<label class="label label-danger">You need all of the above to call</label>';
 
     }
 });
