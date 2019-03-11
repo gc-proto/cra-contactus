f4283043cc7254fdffcb9e5a3bc27674f44bf5aa
jmealing <31008799+jmealing@users.noreply.github.com>
2019-03-11 08:41:21 -0400

    update

--- js/checkboxes.js
+++ js/checkboxes.js
@@ -7,7 +7,7 @@ $(".btnCheck").click(function(event) {
     }
 });
 
-$(".btnCheck").click(function(event) {
+$(".btnCheck2").click(function(event) {
     if ($('.beforeCall2 input[type="checkbox"]').not(':checked').length == 0) {
 				document.getElementById("check-message").innerHTML = '<label class="label label-success">You are ready to call</label>';
     } else {
