568849dc9a97c83926bf068c8e2d468fb155ff25
jmealing <31008799+jmealing@users.noreply.github.com>
2019-03-11 08:37:51 -0400

    need v2 when there's two on a page

--- js/checkboxes.js
+++ js/checkboxes.js
@@ -6,3 +6,12 @@ $(".btnCheck").click(function(event) {
 
     }
 });
+
+$(".btnCheck").click(function(event) {
+    if ($('.beforeCall2 input[type="checkbox"]').not(':checked').length == 0) {
+				document.getElementById("check-message").innerHTML = '<label class="label label-success">You are ready to call</label>';
+    } else {
+				document.getElementById("check-message").innerHTML = '<label class="label label-danger">You need all of the above to call</label>';
+
+    }
+});
