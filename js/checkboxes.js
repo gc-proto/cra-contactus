68df2921fac42108e8e687c9a85881bb62bd7ee0
jmealing <31008799+jmealing@users.noreply.github.com>
2019-03-11 08:43:34 -0400

    adjust script

--- js/checkboxes.js
+++ js/checkboxes.js
@@ -9,9 +9,9 @@ $(".btnCheck").click(function(event) {
 
 $(".btnCheck2").click(function(event) {
     if ($('.beforeCall2 input[type="checkbox"]').not(':checked').length == 0) {
-				document.getElementById("check-message").innerHTML = '<label class="label label-success">You are ready to call</label>';
+				document.getElementById("check-message2").innerHTML = '<label class="label label-success">You are ready to call</label>';
     } else {
-				document.getElementById("check-message").innerHTML = '<label class="label label-danger">You need all of the above to call</label>';
+				document.getElementById("check-message2").innerHTML = '<label class="label label-danger">You need all of the above to call</label>';
 
     }
 });
