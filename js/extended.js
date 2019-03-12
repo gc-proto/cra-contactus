dfb3d3ebf80b4989f87026eabc6c8d2bda801d56
Jennifer Mealing <jennifermealingdesign@gmail.com>
2019-03-12 14:40:20 -0400

    js changes to support other phone lines

--- js/extended.js
+++ js/extended.js
@@ -31,3 +31,37 @@ $(".need-date").each(function(index, element) {
   }
 });
 });
+
+$(document).ready(function() {
+var holidays = {};
+holidays["4-19"] = true;
+holidays["4-21"] = true;
+holidays["4-22"] = true;
+
+
+$(".need-date-2").each(function(index, element) {
+  var el = $(element);
+  var date = new Date();
+  var hour = date.getHours();
+  var minutes = date.getMinutes();
+  var day = date.getDay();
+  var month = date.getMonth();
+  var dayOfMonth = date.getDate();
+
+  var lineOpen = Number(el.data("day" + day + "-open"));
+  var lineClose = Number(el.data("day" + day + "-close"));
+
+  var closeClock = (lineClose - 12);
+  var nextDay = Number(el.data("day" + (day + 1) + "-open"));
+
+
+  console.log(lineOpen, lineClose);
+  var isHoliday = holidays["" + month + "-" + dayOfMonth + ""];
+
+  if (!isHoliday && hour >= lineOpen && hour <= lineClose) {
+    el.find(".open-or-closed-2").html('<label class="label label-success">Open until ' + closeClock +'PM</label>');
+  } else {
+    el.find(".open-or-closed-2").html('<label class="label label-danger">Closed until ' + nextDay + 'AM</label>');
+  }
+});
+});
