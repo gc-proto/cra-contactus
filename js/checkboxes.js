b418387e621238dbc736c496a8cba477050c37e5
jmealing <31008799+jmealing@users.noreply.github.com>
2019-03-12 19:35:05 -0400

    add more checkboxes

--- js/checkboxes.js
+++ js/checkboxes.js
@@ -26,3 +26,22 @@ $(".btnCheck3").click(function(event) {
 
     }
 });
+
+
+$(".btnCheck4").click(function(event) {
+    if ($('.beforeCall4 input[type="checkbox"]').not(':checked').length == 0) {
+				document.getElementById("check-message4").innerHTML = '<label class="label label-success">You are ready to call</label>';
+    } else {
+				document.getElementById("check-message4").innerHTML = '<label class="label label-danger">You need all of the above to call</label>';
+
+    }
+});
+
+$(".btnCheck5").click(function(event) {
+    if ($('.beforeCall5 input[type="checkbox"]').not(':checked').length == 0) {
+				document.getElementById("check-message5").innerHTML = '<label class="label label-success">You are ready to call</label>';
+    } else {
+				document.getElementById("check-message5").innerHTML = '<label class="label label-danger">You need all of the above to call</label>';
+
+    }
+});
